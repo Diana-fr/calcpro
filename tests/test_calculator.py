a4289ee4f43c08331cc61625d48e2c810b9465ea
Diana <tern75@list.ru>
2025-03-29 21:40:42 +0300

    Fix import issue in tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,9 @@
+import sys
+import os
 import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'C:/Users/1/calcpro/src')))
+
 from calculator import add, subtract, multiply, divide
 
 def test_add_positive_numbers():
